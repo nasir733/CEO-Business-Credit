bcec67d5c3b61831e96d198e4c3c7fd259afc001
Thasanthan-codethamizha <sivakumarthasanthan@gmail.com>
2021-12-01 23:34:38 +0530

    deploy

--- getdinerotoday/settings/development.py
+++ getdinerotoday/settings/development.py
@@ -5,10 +5,8 @@ config = dotenv_values(".env")
 DEBUG = True
 
 SECRET_KEY = '!nz#yq7*eo@3d*1(=z=f0jd-&uq!2j#ivns(shit7*b0d_h%ki'
-STRIPE_PUBLISHABLE_KEY = os.environ.get(
-    'STRIPE_PUBLISHABLE_KEY') if not DEBUG else config['STRIPE_PUBLISHABLE_KEY']
-STRIPE_SECRET_KEY = os.environ.get(
-    'STRIPE_SECRET_KEY') if not DEBUG else config['STRIPE_SECRET_KEY']
+STRIPE_PUBLISHABLE_KEY ="pk_test_51GadadExWTjX75uFcS3TQPfUUSt3dkT1Bh92FLcpB3gBUlTI40JU2xp1VPCzktdISjy0dtM14E6WxZP7IoSxlZK100KQEFLvyV"
+STRIPE_SECRET_KEY = "sk_test_51GadadExWTjX75uFGi8HJDnVtz1xsoPqSlVu5C1HQNK5DjdN9ANTXlxinjF9hF7UffAcRpwfOcDtXbxw4jTi8jIk00TqleQ5pg"
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
     # 'SHOW_TOOLBAR_CALLBACK': True,
